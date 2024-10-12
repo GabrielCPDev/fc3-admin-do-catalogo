c286f93dad0e897a5d568731d124b3c7d131ddbe
GabrielCPDev <gabrielcpcontato@outlook.com>
2024-10-12 07:38:06 -0300

    criando criando JpaRepository da categoria

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,16 +1,33 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME,"test");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME,"dev");
         SpringApplication.run(WebServerConfig.class);
 
+    }
+
+    @Bean
+    public ApplicationRunner runner(CategoryRepository repository){
+        return  args -> {
+            List<CategoryJpaEntity> categories = repository.findAll();
+            var filmes = Category.newCategory("Filmes", null,true);
+            repository.saveAndFlush(CategoryJpaEntity.from(filmes));
+            repository.deleteAll();
+        };
     }
 }
